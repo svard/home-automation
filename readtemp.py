5797ed2e6824603ee2cdf0515dba6c9d6090f6cb
Kristofer Svärd <kristofer.svard@gmail.com>
2013-03-24 21:47:51 +0100

    new buffer feature when the server doesn't answer the temperature will be buffered locally until contact with the server is restored again. logging to file instead of stdout.

--- readtemp.py
+++ readtemp.py
@@ -9,8 +9,13 @@ import json
 import re
 import subprocess
 import urllib2
+import sys
+import logging
+import MySQLdb as mysql
 from datetime import datetime
 from optparse import OptionParser
+
+logging.basicConfig(format='%(levelname)s:%(asctime)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S', filename='/home/kristofer/logs/readtemp.log', level=logging.DEBUG)
 
 def readTemperature(deviceId):
     tempDict = {}
@@ -35,11 +40,93 @@ def reportTemperature(temp, name, host):
     
     try:
         f = urllib2.urlopen(post)
+        if __haveBuffer():
+            sendBuffer(host)
     except urllib2.URLError, e:
-        print "Failed to send measurement. Response code %d" % (e.code)
+        logging.error("Failed to send measurement.")
+        bufferTemperature(temp, name)
     else:
         f.close()
-        print "Reported a temp. of " + str(temp["temp"]) + " degrees at " + temp["date"]
+        logging.info("Reported a temp. of " + str(temp["temp"]) + " degrees at " + temp["date"])
+        
+def bufferTemperature(temp, name):
+    dbConn = __connectDb()
+    
+    try:
+        cursor = dbConn.cursor()
+        logging.debug("INSERT INTO tempbuffer(device_id, device_name, temperature, date, date_str) VALUES(%d, '%s', %.1f, %d, '%s')" % (temp["id"], name, temp["temp"], temp["unixdate"], temp["date"]))  
+        cursor.execute("INSERT INTO tempbuffer(device_id, device_name, temperature, date, date_str) VALUES(%d, '%s', %.1f, %d, '%s')" % (temp["id"], name, temp["temp"], temp["unixdate"], temp["date"]))
+        dbConn.commit()        
+    except mysql.Error, e:
+        logging.error("Error buffering to db %d: %s" % (e.args[0],e.args[1]))
+        dbConn.rollback()
+        sys.exit(1)
+    finally:
+        if dbConn:
+            dbConn.close()
+            
+def sendBuffer(host):
+    dbConn = __connectDb()
+    
+    try:
+        cursor = dbConn.cursor(mysql.cursors.DictCursor)
+        cursor.execute("SELECT * FROM tempbuffer")
+        
+        rows = cursor.fetchall()
+        for row in rows:
+            data = json.dumps({"deviceId": row["device_id"], "deviceName": row["device_name"], "temperature": row["temperature"], "date": row["date"], "dateStr": row["date_str"]})
+            try:
+                post = urllib2.Request(host, data, {"Content-Type": "application/json"})
+                f = urllib2.urlopen(post)
+            except urllib2.URLError, e:
+                logging.error("Failed to send measurement.")
+            finally:
+                logging.info("Reported a temp. of " + str(row["temperature"]) + " degrees at " + row["date_str"])
+                logging.debug("DELETE FROM tempbuffer WHERE buffer_id=%d" % row["buffer_id"])
+                cursor.execute("DELETE FROM tempbuffer WHERE buffer_id=%d" % row["buffer_id"])
+                dbConn.commit()
+    except mysql.Error, e:
+        logging.error("Error reading from db %d: %s" % (e.args[0],e.args[1]))
+        sys.exit(1)
+    finally:
+        dbConn.close()
+        f.close()
+    
+def __connectDb():
+    dbConn = None
+    
+    try:
+        dbConn = mysql.connect('192.168.0.192', 'kristofer', 'gu7101L2', 'automation')
+        
+    except mysql.Error, e:
+        logging.error("Failed to connect to db %d: %s" % (e.args[0],e.args[1]))
+        sys.exit(1)
+        
+    return dbConn
+            
+def __haveBuffer():
+    dbConn = __connectDb()
+    
+    try:
+        cursor = dbConn.cursor()
+        
+        cursor.execute("SELECT * FROM tempbuffer")
+        
+        if int(cursor.rowcount) > 0:
+            logging.debug("%d buffered temperatures exists." % int(cursor.rowcount))
+            return True
+        else:
+            logging.debug("No buffered temperatures exists.")
+            return False
+        
+    except mysql.Error, e:
+        logging.error("Error reading db %d: %s" % (e.args[0],e.args[1]))
+        sys.exit(1)
+        
+    finally:
+        if dbConn:
+            dbConn.close()
+        
 
 if __name__ == '__main__':
     parser = OptionParser()
@@ -66,3 +153,5 @@ if __name__ == '__main__':
     
     result = readTemperature(deviceId)
     reportTemperature(result, deviceName, host)
+    
+    sys.exit(0)
